5765b31f6427524c71a3f2f40767bafa76e87f49
habib <habibdev96@gmail.com>
2021-06-05 09:19:08 -0600

    refactored companies section into carousel

--- js/main.js
+++ js/main.js
@@ -8,3 +8,21 @@ const preloader = () => {
   });
 };
 preloader();
+
+const companiesCarousel = () => {
+  const companiesCarouselConfig = {
+    type: "carousel",
+    autoplay: 2000,
+    perView: 4,
+    breakpoints: {
+      980: {
+        perView: 3,
+      },
+      400: {
+        perView: 2,
+      },
+    },
+  };
+  new Glide(".glide", companiesCarouselConfig).mount();
+};
+companiesCarousel();
